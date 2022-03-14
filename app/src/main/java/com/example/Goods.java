5cca45d672953188850a961afa1efc14038fc4f1
goldia <goldia2314@gmail.com>
2022-03-14 14:49:29 +0700

    finish2

---
+++ app/src/main/java/com/example/Goods.java
@@ -0,0 +1,42 @@
+package com.example;
+
+public class Goods {
+
+    private String name;
+    private String shopName;
+    private int imgGoods;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getShopName() {
+        return shopName;
+    }
+
+    public void setShopName(String shopName) {
+        this.shopName = shopName;
+    }
+
+    public int getImgGoods() {
+        return imgGoods;
+    }
+
+    public void setImgGoods(int imgGoods) {
+        this.imgGoods = imgGoods;
+    }
+
+    public Goods() {
+
+    }
+
+    public Goods(String name, String shopName, int imgGoods) {
+        this.name = name;
+        this.shopName = shopName;
+        this.imgGoods = imgGoods;
+    }
+}
